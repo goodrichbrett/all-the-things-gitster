ac75156124652376a602fd0051cfc36899f2ad5c
goodrichbrett <brettgoodrich@outlook.com>
2020-09-11 10:48:05 -0500

    fix bretts thing page

--- src/components/BrettThing/BrettThing.jsx
+++ src/components/BrettThing/BrettThing.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
+// const BrettThing = (props) => {
+// 	return (
+// 		<>
+// 			<div class="card" style={{ width: '18rem' }}>
+// 				<div class="card-body">
+// 					<h5 class="card-title">{props.thing.name}</h5>
+// 					<p class="card-text">{props.thing.attribute}</p>
+// 					<a href="/" class="btn btn-primary">
+// 						Go Back
+// 					</a>
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// };
+
 const BrettThing = (props) => {
 	return (
 		<>
 			<div class="card" style={{ width: '18rem' }}>
 				<div class="card-body">
 					<h5 class="card-title">{props.thing.name}</h5>
-					<p class="card-text">{props.thing.attribute}</p>
+					<p class="card-text">{props.thing.attributes}</p>
 					<a href="/" class="btn btn-primary">
 						Go Back
 					</a>
